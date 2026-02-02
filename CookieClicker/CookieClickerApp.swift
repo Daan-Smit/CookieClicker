a8477c7918bff8956ecfd214993fb65b77b8fcf3
Daan Smit <daan.smit@kpnmail.nl>
2026-02-02 17:37:59 +0100

    Initial Commit

---
+++ CookieClicker/CookieClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookieClickerApp.swift
+//  CookieClicker
+//
+//  Created by DaanSmit on 02/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CookieClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
